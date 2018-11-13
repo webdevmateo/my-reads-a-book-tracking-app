6e3edc488818124040ce9bc8fafc6da2f7748c25
Matthew Smith <mateoelcule@gmail.com>
2018-11-13 11:01:01 -0800

    Added the shelf name as the default value in each book's select menu and did some general error handling in the 'Book' component as well.

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,12 +7,13 @@ class Book extends React.Component {
           const book = this.props.book;
 
           return (
+
                <li>
                  <div className="book">
                    <div className="book-top">
-                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
+                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail || book.imageLinks.smallThumbnail || ''}")` }}></div>
                      <div className="book-shelf-changer">
-                       <select>
+                       <select value={book.shelf || 'none'}>
                          <option value="move" disabled>Move to...</option>
                          <option value="currentlyReading">Currently Reading</option>
                          <option value="wantToRead">Want to Read</option>
@@ -22,7 +23,7 @@ class Book extends React.Component {
                      </div>
                    </div>
                    <div className="book-title">{book.title}</div>
-                   <div className="book-authors">{book.authors.join(' | ')}</div>
+                   <div className="book-authors">{book.authors.join(' | ') || 'No author listed'}</div>
                  </div>
                </li>
           )
