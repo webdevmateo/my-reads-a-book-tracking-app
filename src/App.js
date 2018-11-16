c390689f7ab14b31609d7b01b582731023345e80
Matthew Smith <mateoelcule@gmail.com>
2018-11-16 11:40:31 -0800

    Modified syntax of 'UpdateShelf' method to fix error that was preventing shelves from being updated live, without page refreshes from the Search page to the Main page.

--- src/App.js
+++ src/App.js
@@ -25,14 +25,11 @@ class BooksApp extends React.Component {
   updateShelf = (book, shelf) => {
     BooksAPI.update(book, shelf)
     .then((response) => {
-      console.log(response);
-      console.log(book);
-      console.log(shelf);
       book.shelf = shelf;
       this.setState((state) => ({
         books: state.books.filter((b) => {
-          return b.id !== book.id.concat([book]);
-        })
+          return (b.id !== book.id)
+        }).concat([book])
       }))
     })
   }
